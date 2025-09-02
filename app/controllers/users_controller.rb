7810e4a773dea97f7c6abfb199a186ff9446b49e
Farah Afifah <farahafifah325@gmail.com>
2025-09-02 11:39:33 +0900

    Issue#2 自己紹介文を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -63,8 +63,10 @@ class UsersController < ApplicationController
   private
 
     def user_params
+      Rails.logger.debug "[DEBUG] --------------------------"
+      Rails.logger.debug "[DEBUG] params: #{params}"
       params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
+                                   :password_confirmation, :introduction)
     end
 
     # beforeフィルタ
