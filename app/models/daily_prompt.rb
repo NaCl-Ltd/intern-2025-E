1d2cd9e99a8e9393d40cf898f45f78f996fe79b0
Farah Afifah <farahafifah325@gmail.com>
2025-09-04 15:32:14 +0900

    日替わりのお題の追加

--- app/models/daily_prompt.rb
+++ app/models/daily_prompt.rb
@@ -1,2 +1,7 @@
 class DailyPrompt < ApplicationRecord
+  def self.today_prompt
+    today_hash = Date.today.to_s.hash 
+    ids_size = DailyPrompt.pluck(:id).count 
+    DailyPrompt.find(today_hash%ids_size)
+  end
 end
