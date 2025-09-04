74943bbfc50c5694f9f6512787f18aab820f0a79
Farah Afifah <farahafifah325@gmail.com>
2025-09-04 13:51:59 +0900

    Daily Prompts Model作りました

---
+++ app/models/daily_prompt.rb
@@ -0,0 +1,2 @@
+class DailyPrompt < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_09_02_010602) do
+ActiveRecord::Schema[7.0].define(version: 2025_09_04_042414) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,11 +42,19 @@ ActiveRecord::Schema[7.0].define(version: 2025_09_02_010602) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "daily_prompts", force: :cascade do |t|
+    t.text "en"
+    t.text "ja"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "pinned"
     t.index ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_microposts_on_user_id"
   end
